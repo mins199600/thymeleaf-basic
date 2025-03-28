a67dba44cbc329938d8b145c8d99eef3e37ef8d5
sumin <mins19960091@gmail.com>
2025-03-28 10:13:19 +0900

    mvc2 : Spring EL 변수활용

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package hello.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -19,5 +25,37 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "<b>Hello World!</b>");
         return "/basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User sumin = new User("sumin", 10);
+        User moonsu = new User("moonsu", 20);
+
+        List<User> list =new ArrayList<>();
+        list.add(sumin);
+        list.add(moonsu);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("sumin", sumin);
+        map.put("moonsu", moonsu);
+
+        model.addAttribute("user", sumin);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    //임시유저
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
